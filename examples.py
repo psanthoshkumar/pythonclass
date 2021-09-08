e580a2768a8c579b77a32a76d60b9624f495190e
varma <varma@varmas-Mac-mini.local>
2021-09-07 19:37:15 -0700

    day 2 work notes

---
+++ examples.py
@@ -0,0 +1,20 @@
+# def sum(a, b):
+# 	print(a+b)
+
+
+# def multiply(a, b):
+# 	print(a*b)
+
+
+# sum(10, 20)
+# multiply(10, 20)
+
+
+def sum(c, d):
+	print("c value: ", c)
+	print("d value: ", d)
+	return c+d
+
+print("this is first line")
+# sum(20, 10)
+print("this is last line")
