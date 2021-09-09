10f53342d1d90ae1c3f023e661b0db191b4d4b01
varma <varma@varmas-Mac-mini.local>
2021-09-08 19:38:51 -0700

    updated with day3 class files

---
+++ examples.py
@@ -0,0 +1,33 @@
+# var = "Hello World"
+
+# #print(var[lowerLimit:upperlimit:stepsize])
+
+# print(var[0:5:1])
+
+# print(var[2:7:1])
+
+# print(var[::-1])
+
+
+
+# var = 12345
+
+# print(str(var)[::-1])
+
+
+
+list1 = [1, 2, 3, "Hello", 3.0, 4+3j, True]
+#		 0  1  2    3       4    5      6
+print(list1)
+# print(type(list1))
+
+print(len(list1))
+
+
+# print(list1[7])
+
+list1.append("World")
+print(list1)
+print(len(list1))
+print(list1[7])
+
