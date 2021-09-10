b380706fa64e1236ab7d9df479474fbd52fbb910
varma <varma@varmas-Mac-mini.local>
2021-09-09 19:28:27 -0700

    day4 class files

---
+++ examples.py
@@ -0,0 +1,37 @@
+# tuple1 = (1, 1.0, "1.0", True)
+
+
+# print(tuple1[1])
+# print(len(tuple1))
+
+
+
+
+# Dict Examples:
+
+# dict1 = dict()
+# print(type(dict1))
+
+
+
+# dict2 = {'name': 'Chay', 'age': 26, 'Univ': 'UNT'}
+
+# # To find length
+# print(len(dict2))
+
+# # to read value
+# print(dict2['name'])
+
+
+dict3 = {
+			'name': 'Chay', 
+			'age': 26, 
+			'Univ': 'UNT', 
+			'name': 'Chaitanya', 
+			'name': 'San'
+		}
+
+
+print(type(dict3))
+print(dict3['name'])
+print(dict3['Univ'])
