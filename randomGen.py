8bf635932208452854e029a1de8902f1c9da1d52
varma <varma@varmas-Mac-mini.local>
2021-09-15 19:34:34 -0700

    updated day7 files

---
+++ randomGen.py
@@ -0,0 +1,6 @@
+import random
+from random import randrange, randint, random
+
+# print(random.random())
+# print('OTP is: {}'.format(random.randint(1000, 9999)))
+print(randrange(1, 10))
---
+++ times.py
@@ -0,0 +1,5 @@
+import time
+
+t = time.gmtime()
+
+print(time.asctime(t))
